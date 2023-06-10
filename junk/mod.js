f63af83c369ea9a7697a2b0b81838d4fe55d5631
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2023-06-10 10:22:13 -0400

    Update mod.js

--- junk/mod.js
+++ junk/mod.js
@@ -1,6 +1,5 @@
-core.cookieClicked = function() {
-    core.cookies += 1000;
-    core.cookieBeenClickedTimes += 1;
-    core.totalCookies += 1000;
-    helper.reloadCookieCounter();
-}
+mods.addModData("test mod",{
+    initialization:function() {
+        console.log("HORRAY THE MOD LOADED I ACTUALLY DIDN'T EXPECT THIS YAY!");
+    }
+});
