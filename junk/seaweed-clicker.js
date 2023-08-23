68caddb2f9ccc419fcac648b7bf1d5bc87820e99
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2023-08-23 18:11:36 -0400

    seaweed clicker

---
+++ junk/seaweed-clicker.js
@@ -0,0 +1,8 @@
+mods.addModData("seaweed clicker",{
+    initialization:function() {
+        document.getElementById("cookie").src = "https://images.squarespace-cdn.com/content/v1/5d3a4c1ae616af0001b2aa15/1587409200797-HTWGU79SF1220L47NQCS/keem+edited-8.jpg";
+        personalization.currentClicked = "Seaweed";
+        personalization.currentClickedPlural = "Seaweed";
+        upgrades.descriptions[0] = `Multiplys Keyboard and clicking ${personalization.currentClicked} production by 2`;
+    }
+})
